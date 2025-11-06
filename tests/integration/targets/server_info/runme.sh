1b82ec65a3705a9d1d14292f718283926b07dbd9
Bianca Henderson <bianca@redhat.com>
2025-11-06 13:55:30 -0500

    Remove requirements.txt file, update GH workflow file

--- tests/integration/targets/server_info/runme.sh
+++ tests/integration/targets/server_info/runme.sh
@@ -8,9 +8,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GITHUB_PAT_VALUE="${ANSIBLE_TEST_GITHUB_PAT:-${GITHUB_PAT:-${GITHUB_TOKEN:-${GITHUB_PERSONAL_ACCESS_TOKEN:-}}}}"
 
 INVENTORY="${SCRIPT_DIR}/inventory.yml"
+MANIFEST_PATH="${SCRIPT_DIR}/mcpservers.json"
 
 if [ -n "${GITHUB_PAT_VALUE:-}" ]; then
-    ansible-playbook -i "${INVENTORY}" tasks/main.yml -e "github_pat=${GITHUB_PAT_VALUE}" "$@"
+    ansible-playbook -i "${INVENTORY}" tasks/main.yml -e "ansible_mcp_manifest_path=${MANIFEST_PATH}" -e "github_pat=${GITHUB_PAT_VALUE}" "$@"
 else
-    ansible-playbook -i "${INVENTORY}" tasks/main.yml "$@"
+    ansible-playbook -i "${INVENTORY}" tasks/main.yml -e "ansible_mcp_manifest_path=${MANIFEST_PATH}" "$@"
 fi
