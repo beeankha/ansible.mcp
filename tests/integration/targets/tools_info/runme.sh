cbd17764d78aae13edc7959b414d389af806c159
abikouo <abikouo@redhat.com>
2025-11-17 18:10:54 +0100

    Try to copy from the temporary directory

--- tests/integration/targets/tools_info/runme.sh
+++ tests/integration/targets/tools_info/runme.sh
@@ -3,7 +3,7 @@
 set -eux
 
 function cleanup() {
-    # rm -f ./inventory.yml
+    rm -f ./inventory.yml
     exit 1
 }
 
@@ -13,7 +13,7 @@ export ANSIBLE_ROLES_PATH
 trap 'cleanup "${@}"'  ERR
 
 # Configure test environment
-ansible-playbook setup.yml -e "ansible_mcp_inventory_file_path=./inventory.yml" "$@"
+ansible-playbook setup.yml "$@"
 
 # Run tests
 ansible-playbook test.yml -i inventory.yml "$@"
