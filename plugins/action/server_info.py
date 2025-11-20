e6a0117cfc43958ec756be7434c43a57340c5e58
Bianca Henderson <bianca@redhat.com>
2025-11-20 06:37:16 -0500

    Add `server_info` action plugin (#11)
    * Add server_info action plugin
    
    * Fix linter error
    
    * Remove unnecessary intermediate variable for conn.server_info()
    
    * Add integration test
    
    * Update runme.sh
    
    * Update GH test workflow file to recognize integration tests
    
    * Update GH integration workflow file
    
    * Remove requirements.txt file, update GH workflow file
    
    * Add uv to requirements file for tests, update test workflow files
    
    * Remove integration test from github workflow file
    
    * Add alias file
    
    * More updates to integration test setup
    
    * Change runme.sh to use full path to inventory generation playbook
    
    * Restore generate_inventory.yml step for integration test
    
    * Update error handling
    
    * Update error handling in server_info action plugin
    
    * Update integration test
    
    * Fix pylint issue and add return statements for error handling
    
    * Experiment: Update integration test to include setup role
    
    * Remove conditionals from server_info integration playbook
    
    * Update integration test structure, simplify runme.sh, add .gitignore file
    
    * Remove unused roles/setup directory
    
    * Remove unnecessary mcpservers.json file
    
    * Remove extra blank line to fix sanity test failure
    
    * Update github_mcp_pat lines for setup.yml and server_info_role/tasks/main.yml
    
    * Add aliases file

---
+++ plugins/action/server_info.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025 Red Hat, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import traceback
+
+from ansible.module_utils.connection import Connection
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    """Action plugin to retrieve MCP server information."""
+
+    def run(self, tmp=None, task_vars=None):
+        """Execute the server_info action.
+
+        Returns:
+            dict: Ansible result dictionary containing server_info
+        """
+        if task_vars is None:
+            task_vars = {}
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+        result["changed"] = False
+
+        # Ensure we're using the MCP connection
+        connection_type = self._play_context.connection
+        if not connection_type or not connection_type.endswith(".mcp"):
+            result["failed"] = True
+            result["msg"] = (
+                "Connection type %s is not valid for server_info module, "
+                "please use fully qualified name of MCP connection type."
+            ) % connection_type
+            return result
+
+        # Get socket path from connection
+        socket_path = self._connection.socket_path
+        if socket_path is None:
+            result["failed"] = True
+            result["msg"] = "socket_path is not available from connection"
+            return result
+
+        try:
+            # Use Connection class to call server_info on the connection plugin
+            conn = Connection(socket_path)
+            result["server_info"] = conn.server_info()
+            return result
+
+        except Exception as e:
+            result["failed"] = True
+            result["msg"] = "Failed to retrieve server info: %s" % str(e)
+            result["exception"] = "".join(traceback.format_exception(None, e, e.__traceback__))
+            return result
